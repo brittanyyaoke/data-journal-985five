bf95b7184a0fa128cf5ea0a2d720dbe14aa63077
brittanyyaoke <62843887+brittanyyaoke@users.noreply.github.com>
2020-06-25 21:30:42 +0800

    Add files via upload

---
+++ uagent.py
@@ -0,0 +1,18 @@
+
+import random
+
+# 创建一个代理池
+def get_ua():
+
+    user_agents = [
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36 OPR/26.0.1656.60",
+    "Opera/8.0 (Windows NT 5.1; U; en)",
+    "Mozilla/5.0 (Windows NT 5.1; U; en; rv:1.8.1) Gecko/20061208 Firefox/2.0.0 Opera 9.50",
+    "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/534.16 (KHTML, like Gecko) Chrome/10.0.648.133 Safari/534.16"
+]
+
+    user_agent = random.choice(user_agents) #random.choice(),从列表中随机抽取一个对象
+    return user_agent
+
+
